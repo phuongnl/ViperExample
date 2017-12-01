4b2e4b1a8d51a31f33ec97f4cfff489eaa47e8f1
Phuong Nguyen <phuongnl.hn@gmail.com>
2017-12-01 11:15:47 +0700

    refs task #P2 ~ Update TableViewCell Extension

--- ViperExample/Common/Extension/CollectionViewExt.swift
+++ ViperExample/Common/Extension/CollectionViewExt.swift
@@ -13,18 +13,14 @@ extension UICollectionViewCell: ReusableView {}
 extension UICollectionView {
     
     func register<T: UICollectionViewCell>(_: T.Type) {
-        register(T.self, forCellWithReuseIdentifier: T.defaultReuseIdentifier)
-    }
-    
-    func register<T: UICollectionViewCell>(_: T.Type) where T: NibLoadableView {
         let bundle = Bundle(for: T.self)
         let nib = UINib(nibName: T.nibName, bundle: bundle)
-        register(nib, forCellWithReuseIdentifier: T.defaultReuseIdentifier)
+        register(nib, forCellWithReuseIdentifier: T.nibName)
     }
     
     func dequeueReusableCell<T: UICollectionViewCell>(forIndexPath indexPath: IndexPath) -> T {
-        guard let cell = dequeueReusableCell(withReuseIdentifier: T.defaultReuseIdentifier, for: indexPath) as? T else {
-            fatalError("Could not dequeue cell with identifier: \(T.defaultReuseIdentifier)")
+        guard let cell = dequeueReusableCell(withReuseIdentifier: T.nibName, for: indexPath) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(T.nibName)")
         }
         return cell
     }
